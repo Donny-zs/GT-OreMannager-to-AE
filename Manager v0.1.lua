15af69b8b8a52bce643d04f52d2b56277e509025
Donny-zs <55076088+Donny-zs@users.noreply.github.com>
2021-08-01 14:22:35 +0300

    Update Manager v0.1.lua

--- Manager v0.1.lua
+++ Manager v0.1.lua
@@ -7,3 +7,19 @@ local component = require("component")
 component.me_controller.getItemsInNetwork()
 getItemsInNetwork([filter:table]):table
 
+можно сериализнуть в принципе, но бесперспективно
+лучше прочёсывать через Pair
+
+--------------
+
+обращение к слотам конфигурации интерфейса
+
+getInterfaceConfiguration([slot:number]):table
+Get the configuration of the interface.
+setInterfaceConfiguration([slot:number][, database:address, entry:number[, size:number]]):boolean
+
+пример обращения к датабазе из под транспозера (для сравнения)
+
+component.transposer.compareStackToDatabase(side:number, slot:number, dbAddress:string, dbSlot:number, checkNBT:boolean=false):boolean
+compareStackToDatabase(1,2,db,3,false), где: 1 - сундук сверху, 2 - его второй слот который будет перебираться по хорошему, db переменная содержащая адресс датабазы,
+3 слот этой датабазы, false проверять ли НБТ (урон или тип предмета в греге, вообще там надо проверять, так что там true надо)
