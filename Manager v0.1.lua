5a21b83417d146b120e7a6f70ad7664bcd809b06
Donny-zs <55076088+Donny-zs@users.noreply.github.com>
2021-08-01 13:48:29 +0300

    Update Manager v0.1.lua

--- Manager v0.1.lua
+++ Manager v0.1.lua
@@ -1,2 +1,9 @@
 local component = require("component")
 
+
+
+
+--[[ обращение за всеми предметами в сети в виде таблицы с таблицами
+component.me_controller.getItemsInNetwork()
+getItemsInNetwork([filter:table]):table
+
