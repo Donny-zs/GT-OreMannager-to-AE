3c579934f3af4831bfb8a48803b0879863c842c3
Donny-zs <55076088+Donny-zs@users.noreply.github.com>
2021-08-01 16:58:20 +0300

    Update Manager v0.1.lua

--- Manager v0.1.lua
+++ Manager v0.1.lua
@@ -17,8 +17,17 @@ getInterfaceConfiguration([slot:number]):table
 Get the configuration of the interface.
 setInterfaceConfiguration([slot:number][, database:address, entry:number[, size:number]]):boolean
 
-пример обращения к датабазе из под транспозера (для сравнения)
+пример обращения к датабазе из под транспозера 
 
+(запись)
+component.transposer.store(сторона света транспозера с которой находитс интерфейс, номер слота интерфейса, адрес датабазы, номер слота датабазы )
+вернёт булен, который скажет было там что-то или нет
+
+(очистка)
+component.database.clear(номер слота датабазы)
+вернёт тру если что-то было стёрто
+
+(для сравнения)
 component.transposer.compareStackToDatabase(side:number, slot:number, dbAddress:string, dbSlot:number, checkNBT:boolean=false):boolean
 compareStackToDatabase(1,2,db,3,false), где: 1 - сундук сверху, 2 - его второй слот который будет перебираться по хорошему, db переменная содержащая адресс датабазы,
 3 слот этой датабазы, false проверять ли НБТ (урон или тип предмета в греге, вообще там надо проверять, так что там true надо)
@@ -26,3 +35,6 @@ compareStackToDatabase(1,2,db,3,false), где: 1 - сундук сверху, 2
 Получить в виде таблицы все внутри инвенторя
 component.transposer.getAllStacks(сторона транспозера):getAll()
 
+------- Прочее
+Одноблоки грегтека имеют неиспользуемые игроком слоты с 1 до 4
+Транспозер не может взаимодействовать напрямую с одноблоками грега
